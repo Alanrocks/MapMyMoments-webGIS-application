8433b809697c38ffbee2db6539f3ec664e4f6b4a
Alan Wu <126620008+Alanrocks@users.noreply.github.com>
2023-05-04 02:34:40 -0400

    Update feed.js

--- js/feed.js
+++ js/feed.js
@@ -63,3 +63,15 @@ function getlocation(){
 //   progressbar.parentElement.style.display='flex'
 // });
 // }
+
+var myInput = document.getElementById('addphotobtn');
+
+function sendPic() {
+    var file = myInput.files[0];
+
+    // Send file here either by adding it to a `FormData` object 
+    // and sending that via XHR, or by simply passing the file into 
+    // the `send` method of an XHR instance.
+}
+
+myInput.addEventListener('change', sendPic, false);
